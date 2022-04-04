f007bcf42090e2752dab4a379cf9d67a8a5b4209
kolaso13 <adrbrarare@gmail.com>
2022-04-04 12:36:11 +0200

    v0.57

--- ProyectoFinal-main/src/views/Anime.js
+++ ProyectoFinal-main/src/views/Anime.js
@@ -40,12 +40,30 @@ const Anime = ({ data }) => {
       <Navbar />
       <br />
 
-      {console.log(AnimeData)}
       {AnimeData?.map((ani) => (
-        <div className="card" key={ani.name} id="content">
-          <img alt="" src={ani.image} />
-          <h2>Anime name: {ani.name}</h2>
-          <p>Anime Description: {ani.description}</p>
+        <div className="card" key={ani.name}>
+          <div id="content">
+            <div>
+              <h3>{ani.name}</h3>
+              <hr />
+              <img alt="" src={ani.image} style={{ width: "250px" }} />
+              <h5>Information</h5>
+              <p>Episodes: {ani.chapters} </p>
+              <p>Status: Not yet aired </p>
+              <p>Aired: {ani.date} </p>
+              <p>Premiered: Spring 2022 </p>
+              <p>Broadcast: Saturdays at 23:00 (JST)</p>
+              <p>Producers: TOHO animation</p>
+              <p>Studios: {ani.studio}</p>
+              <p>Genres: {ani.genre} </p>
+              <p>Demographic: Shounen </p>
+              <p>Duration: Unknown </p>
+            </div>
+            <div>
+              <h5>Sinopsis</h5>
+              <p>{ani.description}</p>
+            </div>
+          </div>
         </div>
       ))}
     </div>
