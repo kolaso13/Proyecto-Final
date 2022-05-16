a3376de84b93ffdfba6fa2a433b476ba85937a39
kolaso13 <adrbrarare@gmail.com>
2022-05-16 07:42:48 +0200

    v0.76

--- ProyectoFinal-main/src/views/Profile.js
+++ ProyectoFinal-main/src/views/Profile.js
@@ -45,79 +45,82 @@ const Profile = ({ data }) => {
   //   );
   // }
   return (
-    <div>
+    <>
       <Navbar />
-      <br />
 
-      <div>
-        <div id="contenido">
-          <h3 id="username">Username</h3>
-          <div id="Information">
-            <div id="userInfo">
-              <img
-                src="../img/ShingekinoKyojin.jpg"
-                alt=""
-                style={{ width: "200px", height: "300px" }}
-              />
-              <p>Nombre: Peter</p>
-              <p>Apellido: Plato</p>
-              <p>Edad: 34</p>
-            </div>
-            <div id="userStats">
-              <p>
-                Biografia: La biografía 1​es un tipo o subgénero
-                literario-histórico situado dentro de los géneros
-                "memorialísticos", y a su vez integrados en los ensayísticos.2​
-                Se configura modernamente, sobre todo, con las moralizantes
-                Vidas paralelas de Plutarco y los distintos De viris illustribus
-                y atraviesa la Edad Media característicamente en forma de
-                hagiografía hasta alcanzar la biografía carolingia; se
-                seculariza a partir del Renacimiento y reverdece en el siglo XIX
-                con el Romanticismo, volviéndose luego con el Realismo mucho más
-                riguroso y documental hasta la actualidad.
-              </p>
-              <div id="FavAnimes">
-                <table id="FavAnimesTable">
-                  <thead>
-                    <tr>
-                      <th>#</th>
-                      <th>Image</th>
-                      <th>Anime Title</th>
-                      <th>Score</th>
-                      <th>Type</th>
-                      <th>Episodes</th>
-                      <th>Delete</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {DataPerfil?.map((dat) => {
-                      return (
-                        <tr key={dat.name}>
-                          <td>1</td>
-                          <td>
-                            <img
-                              src={dat.image}
-                              style={{ width: "75px", height: "100px" }}
-                            ></img>
-                          </td>
-                          <td>{dat.name}</td>
-                          <td>{Math.floor(Math.random() * 100) + 1}</td>
-                          <td>TV</td>
-                          <td>{dat.episodes}</td>
-                          <td>x</td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
-              </div>
+      <div id="profileContent">
+        <div id="profileTop">
+          <img
+            id="userAvatar"
+            src={require("../img/profile.png")}
+            alt=""
+            style={{ width: "300px", height: "300px" }}
+          />
+          <h1 id="username">Username</h1>
+        </div>
+        <div id="profileBottom">
+          <div id="userData">
+            <h3>
+              <b>Información</b>
+            </h3>
+            <hr />
+
+            <h5>Nombre: Peter </h5>
+            <h5>Apellido: Plato</h5>
+            <h5>Edad: 34</h5>
+            <h5>Sexo: Masculino</h5>
+            <h5>
+              {" "}
+              Biografia: La biografía es un tipo o subgénero literario-histórico
+              situado dentro de los géneros "memorialísticos", y a su vez
+              integrados en los ensayísticos.
+            </h5>
+          </div>
+          <div id="userAnime">
+            <h3>
+              <b>Animes Favoritos</b>
+            </h3>
+            <hr />
+            <div id="FavAnimesDiv">
+              <table id="FavAnimesTable">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Image</th>
+                    <th>Anime Title</th>
+                    <th>Score</th>
+                    <th>Type</th>
+                    <th>Episodes</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {DataPerfil?.map((dat) => {
+                    return (
+                      <tr key={dat.name}>
+                        <td>1</td>
+                        <td>
+                          <img
+                            src={dat.image}
+                            style={{ width: "75px", height: "100px" }}
+                          ></img>
+                        </td>
+                        <td>{dat.name}</td>
+                        <td>{Math.floor(Math.random() * 100) + 1}</td>
+                        <td>TV</td>
+                        <td>{dat.episodes}</td>
+                        <td>x</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
       </div>
-
       <Footer />
-    </div>
+    </>
   );
 };
 
